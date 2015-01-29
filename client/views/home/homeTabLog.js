9a6d9b4617c0c1a7715606bf1260b93de323c020
Kovács Vince <vincekovacs@hotmail.com>
2015-01-29 06:12:19 +0100

    Log tab base + disable buttons if not connected

--- client/views/home/homeTabLog.js
+++ client/views/home/homeTabLog.js
@@ -1,3 +1,13 @@
-/**
- * Created by van Gogh on 2015.01.29..
- */
+(function (undefined) {
+    "use strict";
+
+    Template.homeTabLog.events(
+        {
+            'click .js-btn-read-log': function () {
+                Meteor.call('mspLog', Session.get('mspConnectedDevice'), function (error, response) {
+                    Session.set('mspLog', response);
+                    notify('Log read', 'success');
+                });
+            }
+        });
+}());
